94f6392e7e7a5b168c044aed3ce1d99317f1c99a
Aadesh Chourasiya <aadeshchourasiya@Aadeshs-MacBook-Air.local>
2026-02-22 17:28:18 +0530

    Enable CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import psycopg2
 from prophet import Prophet
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
